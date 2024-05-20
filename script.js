21d060394f9702c38bf8cecf21539f65942dea4f
poljon2024 <laperalindon@gmail.com>
2024-05-20 07:14:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,353 @@
+const genusList = {
+
+"okra": "Abelmoschus esculentus",
+"sapodilla": "Achras sapota",
+"kiwi": "Actinidia chinensis",
+"button Mushroom": "Agaricus bisporus",
+"mushroom": "Agaricus spp",
+"green Agave": "Agave atrovirens",
+"tung Tree": "Aleurites spp",
+"leek": "Allium ampeloprasum",
+"onion": "Allium cepa",
+"leek": "Allium porrum",
+"garlic": "Allium sativum",
+"chives": "Allium schoenoprasum",
+"aloe Vera": "Aloe vera",
+"galangal": "Alpinia officinarum",
+"cashew": "Anacardium occidentale",
+"pineapple": "Ananas comosus",
+"dill": "Anethum graveolens",
+"caraway": "Anisosciadium lanatum",
+"cherimoya": "Annona reticulata",
+"sugar Apple": "Annona squamosa",
+"chervil": "Anthriscus cerefolium",
+"celery": "Apium graveolens",
+"peanut": "Arachis hypogaea",
+"areca Nut": "Areca catechu",
+"horseradish": "Armoracia rusticana",
+"arracacha": "Arracacia xanthorhiza",
+"tarragon": "Artemisia dracunculus",
+"breadfruit": "Artocarpus altilis",
+"asparagus": "Asparagus officinalis",
+"quebracho": "Aspidosperma spp",
+"brazil Nut": "Bertholletia excelsa",
+"beet": "Beta vulgaris",
+"cabbage": "Brassica oleracea capitata",
+"rapeseed": "Brassica napus",
+"cabbage": "Brassica oleracea",
+"broccoli": "Brassica oleracea Var.Italica",
+"cauliflower": "Brassica oleracea var. botrytis",
+"cabbage": "Brassica oleracea var. capitata",
+"mustard": "Brassica spp",
+"pigeon Pea": "Cajanus cajan",
+"tea": "Camellia sinensis",
+"bell Pepper": "Capsicum annuum",
+"pepper": "Capsicum spp",
+"capsicum":"Capsicumm annuum",
+"papaya": "Carica papaya",
+"caraway": "Carum carvi",
+"pecan": "Carya illinoensis",
+"chestnut": "Castanea sativa",
+"kapok": "Ceiba pentandra",
+"carob": "Ceratonia siliqua",
+"quinoa": "Chenopodium quinoa",
+"chickpea": "Cicer arietinum",
+"endive": "Cichorium endivia",
+"chicory": "Cichorium intybus",
+"cinchona": "Cinchona spp",
+"watermelon": "Citrullus lanatus",
+"orange": "Citrus sinensis",
+"lime": "Citrus aurantifolia",
+"bitter Orange": "Citrus aurantium",
+"bergamot": "Citrus bergamia",
+"pomelo": "Citrus grandis",
+"kaffir Lime": "Citrus hystrix DC",
+"lemon": "Citrus limon",
+"citrus Medica": "Citrus medica",
+"grapefruit": "Citrus paradisi",
+"tangerine": "Citrus reticulata",
+"sweet Orange": "Citrus sinensis",
+"mandarin": "Citrus veticulata",
+"ivy Gourd": "Coccinia grandis",
+"coconut": "Cocos nucifera",
+"coffee": "Coffee spp",
+"taro": "Colocasia spp",
+"congo Jute": "Corchorus olitorius",
+"coriander": "Coriandrum sativum",
+"hazelnut": "Corylus avellana",
+"melon": "Cucumis melo",
+"honeydew Melon": "Cucumis melo Inodorus",
+"armenian Cucumber": "Cucumis melo flexuosus",
+"cucumber": "Cucumis sativus",
+"pumpkin": "Cucurbita pepo",
+"squash": "Cucurbita spp",
+"turmeric": "Curcuma longa",
+"guar": "Cyamopsis tetragonoloba",
+"quince": "Cydonia oblonga",
+"lemongrass": "Cymbopogon spp",
+"artichoke": "Cynara cardunculus",
+"carrot": "Daucus carota",
+"longan": "Dimocarpus longan",
+"yam": "Dioscorea spp",
+"persimmon": "Diospyros kaki",
+"loquat": "Eriobotrya japonica",
+"arugula": "Eruca Sativa",
+"fig": "Ficus carica",
+"fennel": "Foeniculum vulgare",
+"strawberry": "Fragaria spp",
+"maguey": "Furcraea macrophylla",
+"mangosteen": "Garcinia mangostana",
+"soybean": "Glycine max",
+"licorice": "Glycyrrhiza glabra",
+"grape Leaves": "Vitis vinifera",
+"niger Seed": "Guizotia abyssinica",
+"kenaf": "Hibiscus cannabinus",
+"barley": "Hordeum vulgare",
+"hops": "Humulus lupulus",
+"dragon Fruit": "Hylocereus costaricensis",
+"yerba Mate": "Ilex paraguariensis",
+"sweet Potato": "Ipomoea batatas",
+"walnut": "Juglans spp",
+"lettuce": "Lactuca sativa",
+"bottle Gourd": "Lagenaria siceraria",
+"lavender": "Lavandula spp",
+"lentil": "Lens culinaris",
+"cress": "Lepidium sativum",
+"flax": "Linum usitatissimum",
+"litchi": "Litchi chinensis",
+"lupin": "Lupinus spp",
+"macadamia": "Macadamia spp",
+"apple": "Malus domestica",
+"crab Apple": "Malus sylvestris",
+"mango": "Mangifera indica",
+"yuca": "Manihot esculenta",
+"chico Fruit": "Manilkara zapota",
+"arrowroot": "Maranta arundinacea",
+"mint": "Mentha spp",
+"peppermint": "Mentha × piperita",
+"medlar": "Mespilus germanica",
+"bitter Melon": "Momordica charantia",
+"moringa": "Moringa oleifera",
+"mulberry": "Morus spp",
+"banana": "Musa acuminata",
+"plantain": "Musa paradisiaca",
+"abaca": "Musa textilis",
+"rambutan": "Nephelium lappaceum",
+"tobacco": "Nicotiana tabacum",
+"basil": "Ocimum basilicum",
+"olive": "Olea europaea",
+"sainfoin": "Onobrychis viciifolia",
+"marjoram": "Origanum majorana",
+"parsnip": "Pastinaca sativa",
+"perilla": "Perilla spp",
+"avocado": "Persea americana",
+"parsley": "Petroselinum crispum",
+"common Bean": "Phaseolus vulgaris",
+"date": "Phoenix dactylifera",
+"new Zealand Flax": "Phormium tenax",
+"indian Gooseberry": "Phyllanthus emblica",
+"goldenberry": "Physalis peruviana",
+"groundcherry": "Physalis spp",
+"anise": "Pimpinella anisum",
+"black Pepper": "Piper nigrum",
+"pistachio": "Pistacia spp",
+"pea": "Pisum sativum",
+"chinese Potato": "Plectranthus rotundifolius",
+"purslane": "Portulaca oleracea",
+"tinda": "Praecitrullus fistulosus",
+"apricot": "Prunus armeniaca",
+"plum": "Prunus domestica",
+"almond": "Prunus dulcis",
+"peach": "Prunus persica",
+"cherry": "Prunus spp",
+"guava": "Psidium guajava",
+"pomegranate": "Punica granatum",
+"pear": "Pyrus communis",
+"radish": "Raphanus sativus",
+"redcurrant": "Ribes rubrum",
+"currant": "Ribes spp",
+"wild Rose": "Rosa davurica",
+"rosemary": "Rosmarinus officinalis",
+"bramble": "Rubus spp",
+"raspberry": "Rubus spp",
+"sugarcane": "Saccharum officinarum",
+"sage": "Salvia officinalis",
+"black Salsify": "Scorzonera hispanica",
+"chayote": "Sechium edule",
+"sesame": "Sesamum indicum",
+"potato": "Solanum tuberosum",
+"tomato": "Solanum lycopersicum",
+"eggplant": "Solanum melongena",
+"pepino": "Solanum muricatum",
+"argel": "Solenostemma argel",
+"sorghum": "Sorghum bicolor",
+"spinach": "Spinacia oleracea",
+"sweet Basil": "Ocimum basilicum",
+"tamarind": "Tamarindus indica",
+"desert Truffle": "Terfeziaceae spp",
+"cocoa": "Theobroma cacao",
+"thyme": "Thymus vulgaris",
+"snake Gourd": "Trichosanthes cucumerina",
+"trichosanthes": "Trichosanthes spp",
+"clover": "Trifolium spp",
+"fenugreek": "Trigonella foenum-graecum",
+"wheat": "Triticum spp",
+"blueberry": "Vaccinium spp",
+"corn Salad": "Valerianella locusta",
+"vanilla": "Vanilla planifolia",
+"fava Bean": "Vicia faba",
+"common Vetch": "Vicia sativa",
+"cowpea": "Vigna unguiculata",
+"grape": "Vitis vinifera",
+"tannia": "Xanthosoma sagittifolium",
+"corn": "Zea mays",
+"ginger": "Zingiber officinale",
+"jujube": "Ziziphus jujuba",
+"kumquat": "Fortunella margarita",
+"vigna": "Vigna spp",
+"passion fruit" : "Passiflora edulis",
+"blackberries" : "Rubus fruticosus",
+"blackberry" : "Rubus fruticosus",
+"beans" : "Phaseolus vulgaris",
+"courgettes" : "Cucurbita pepo",
+
+    
+    // Add more fruits and vegetables with their genus here
+};
+
+function searchGenus() {
+    const input = document.getElementById('searchInput').value.toLowerCase();
+    const genus = genusList[input];
+    document.getElementById('genusDisplay').innerText = genus ? genus : 'Not Found';
+}
+
+function addToTable() {
+    const genus = document.getElementById('genusDisplay').innerText;
+    const name = document.getElementById('searchInput').value; 
+    const quantity = document.getElementById('quantityInput').value;
+    const kilograms = document.getElementById('kilogramInput').value;
+
+    if (genus &&name && quantity && kilograms) {
+        const table = document.getElementById('resultTable').getElementsByTagName('tbody')[0];
+        const newRow = table.insertRow();
+
+        const cell1 = newRow.insertCell(0);
+        const cell2 = newRow.insertCell(1);
+        const cell3 = newRow.insertCell(2);
+        const cell4 = newRow.insertCell(3);
+        const cell5 = newRow.insertCell(4);
+
+        cell1.innerText = genus;
+        cell2.innerText = name;
+        cell3.innerText = quantity;
+        cell4.innerText = kilograms;
+
+        const copyButton = document.createElement('button');
+        copyButton.textContent = 'Copy';
+        copyButton.onclick = function() {
+            copyToClipboard(genus, copyButton);
+        };
+        cell5.appendChild(copyButton);
+
+        cell5.appendChild(document.createTextNode(' '));
+
+     const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.onclick = function() {
+            deleteRow(newRow);
+        };
+        cell5.appendChild(deleteButton);
+
+        updateSummary();
+    }
+}
+
+function deleteRow(row) {
+    const table = document.getElementById('resultTable').getElementsByTagName('tbody')[0];
+    table.deleteRow(row.rowIndex - 1); // Subtract 1 to adjust for header row
+    updateSummary();
+}
+
+function updateSummary() {
+    const table = document.getElementById('resultTable').getElementsByTagName('tbody')[0];
+    let totalQuantity = 0;
+    let totalKilograms = 0;
+
+    for (let row of table.rows) {
+        const quantity = parseFloat(row.cells[2].innerText);
+        const kilograms = parseFloat(row.cells[3].innerText);
+        totalQuantity += quantity;
+        totalKilograms += kilograms;
+    }
+
+    document.getElementById('totalQuantity').innerText = totalQuantity; // Round to 2 decimal places
+    document.getElementById('totalKilograms').innerText = totalKilograms; // Round to 2 decimal places
+}
+
+function calculateDifference() {
+    const totalQuantity = parseInt(document.getElementById('totalQuantity').innerText);
+    const totalKilograms = parseFloat(document.getElementById('totalKilograms').innerText);
+    const randomQuantity = parseInt(document.getElementById('randomQuantityInput').value);
+    const randomKilograms = parseFloat(document.getElementById('randomKilogramInput').value);
+
+    const quantityDifference = randomQuantity - totalQuantity;
+    const kilogramsDifference = randomKilograms - totalKilograms;
+
+    document.getElementById('quantityDifference').innerText = quantityDifference;
+    document.getElementById('kilogramsDifference').innerText = kilogramsDifference;
+}
+
+// Add event listener to trigger search on "Enter" key press
+document.getElementById('searchInput').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        searchGenus();
+    }
+});
+
+
+function clearData() {
+    // Clear the search input and genus display
+    document.getElementById('searchInput').value = '';
+    document.getElementById('genusDisplay').innerText = '';
+
+    // Clear the input fields for quantity and kilograms
+    document.getElementById('quantityInput').value = '';
+    document.getElementById('kilogramInput').value = '';
+
+    // Clear the result table
+    const table = document.getElementById('resultTable').getElementsByTagName('tbody')[0];
+    table.innerHTML = '';
+
+    // Clear the summary section
+    document.getElementById('totalQuantity').innerText = '0';
+    document.getElementById('totalKilograms').innerText = '0';
+
+    // Clear the random quantity and kilograms input fields
+    document.getElementById('randomQuantityInput').value = '';
+    document.getElementById('randomKilogramInput').value = '';
+
+    // Clear the difference in quantity and kilograms
+    document.getElementById('quantityDifference').innerText = '0';
+    document.getElementById('kilogramsDifference').innerText = '0';
+}
+function copyToClipboard(text, button) {
+    const tempInput = document.createElement('input');
+    tempInput.value = text;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+    
+    // Change appearance of button
+    button.textContent = 'Copied';
+    button.disabled = true;
+    button.style.backgroundColor = '#7FFF00'; // Change button color to green
+    
+    // Reset appearance after a short delay
+    setTimeout(function() {
+        button.textContent = 'Copy';
+        button.disabled = false;
+        button.style.backgroundColor = ''; // Reset button color
+    }, 1000); // Reset appearance after 2 seconds
+}
+
