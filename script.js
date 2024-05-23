e7daa7cc317e786edcdd31b1fc9a16b3d3477c6f
poljon2024 <laperalindon@gmail.com>
2024-05-23 09:31:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,226 +1,224 @@
 const genusList = {
 
-"okra": "Abelmoschus esculentus",
-"ladies finger": "Abelmoschus esculentus",
-"sapodilla": "Achras sapota",
-"kiwi": "Actinidia chinensis",
-"button Mushroom": "Agaricus bisporus",
-"mushroom": "Agaricus spp",
-"green agave": "Agave atrovirens",
-"tung Tree": "Aleurites spp",
-"leek": "Allium ampeloprasum",
-"onion": "Allium cepa",
-"leek": "Allium porrum",
-"garlic": "Allium sativum",
-"chives": "Allium schoenoprasum",
-"aloe vera": "Aloe vera",
-"galangal": "Alpinia officinarum",
-"cashew": "Anacardium occidentale",
-"pineapple": "Ananas comosus",
-"dill": "Anethum graveolens",
-"caraway": "Anisosciadium lanatum",
-"cherimoya": "Annona reticulata",
-"sugar Apple": "Annona squamosa",
-"chervil": "Anthriscus cerefolium",
-"celery": "Apium graveolens",
-"peanut": "Arachis hypogaea",
-"areca Nut": "Areca catechu",
-"horseradish": "Armoracia rusticana",
-"arracacha": "Arracacia xanthorhiza",
-"tarragon": "Artemisia dracunculus",
-"breadfruit": "Artocarpus altilis",
-"asparagus": "Asparagus officinalis",
-"quebracho": "Aspidosperma spp",
-"brazil Nut": "Bertholletia excelsa",
-"beet": "Beta vulgaris",
-"cabbage": "Brassica oleracea capitata",
-"rapeseed": "Brassica napus",
-"cabbage": "Brassica oleracea",
-"broccoli": "Brassica oleracea Var.Italica",
-"cauliflower": "Brassica oleracea var. botrytis",
-"cabbage": "Brassica oleracea var. capitata",
-"mustard": "Brassica spp",
-"pigeon Pea": "Cajanus cajan",
-"tea": "Camellia sinensis",
-"bell pepper": "Capsicum annuum",
-"pepper": "Capsicum spp",
-"capsicum":"Capsicumm annuum",
-"papaya": "Carica papaya",
-"caraway": "Carum carvi",
-"pecan": "Carya illinoensis",
-"chestnut": "Castanea sativa",
-"kapok": "Ceiba pentandra",
-"carob": "Ceratonia siliqua",
-"quinoa": "Chenopodium quinoa",
-"chickpea": "Cicer arietinum",
-"endive": "Cichorium endivia",
-"chicory": "Cichorium intybus",
-"cinchona": "Cinchona spp",
-"watermelon": "Citrullus lanatus",
-"orange": "Citrus sinensis",
-"lime": "Citrus aurantifolia",
-"bitter Orange": "Citrus aurantium",
-"bergamot": "Citrus bergamia",
-"pomelo": "Citrus grandis",
-"kaffir Lime": "Citrus hystrix DC",
-"lemon": "Citrus limon",
-"citrus medica": "Citrus medica",
-"grapefruit": "Citrus paradisi",
-"tangerine": "Citrus reticulata",
-"sweet orange": "Citrus sinensis",
-"mandarin": "Citrus veticulata",
-"ivy gourd": "Coccinia grandis",
-"tindly": "Coccinia grandis",
-"coconut": "Cocos nucifera",
-"coffee": "Coffee spp",
-"taro": "Colocasia spp",
-"congo jute": "Corchorus olitorius",
-"coriander": "Coriandrum sativum",
-"hazelnut": "Corylus avellana",
-"melon": "Cucumis melo",
-"honeydew melon": "Cucumis melo Inodorus",
-"armenian cucumber": "Cucumis melo flexuosus",
-"cucumber": "Cucumis sativus",
-"pumpkin": "Cucurbita pepo",
-"ash gourd": "Benincasa Hispida", 
-"squash": "Cucurbita spp",
-"chow chow": "Cucurbita spp",
-"turmeric": "Curcuma longa",
-"guar": "Cyamopsis tetragonoloba",
-"quince": "Cydonia oblonga",
-"lemongrass": "Cymbopogon spp",
-"artichoke": "Cynara cardunculus",
-"carrot": "Daucus carota",
-"longan": "Dimocarpus longan",
-"yam": "Dioscorea spp",
-"persimmon": "Diospyros kaki",
-"loquat": "Eriobotrya japonica",
-"arugula": "Eruca Sativa",
-"fig": "Ficus carica",
-"fennel": "Foeniculum vulgare",
-"strawberry": "Fragaria spp",
-"maguey": "Furcraea macrophylla",
-"mangosteen": "Garcinia mangostana",
-"soybean": "Glycine max",
-"licorice": "Glycyrrhiza glabra",
-"grape leaves": "Vitis vinifera",
-"niger Seed": "Guizotia abyssinica",
-"kenaf": "Hibiscus cannabinus",
-"barley": "Hordeum vulgare",
-"hops": "Humulus lupulus",
-"dragon fruit": "Hylocereus costaricensis",
-"yerba mate": "Ilex paraguariensis",
-"sweet potato": "Ipomoea batatas",
-"walnut": "Juglans spp",
-"lettuce": "Lactuca sativa",
-"bottle gourd": "Lagenaria siceraria",
-"lavender": "Lavandula spp",
-"lentil": "Lens culinaris",
-"cress": "Lepidium sativum",
-"flax": "Linum usitatissimum",
-"litchi": "Litchi chinensis",
-"lupin": "Lupinus spp",
-"macadamia": "Macadamia spp",
-"apple": "Malus domestica",
-"crab Apple": "Malus sylvestris",
-"mango": "Mangifera indica",
-"yuca": "Manihot esculenta",
-"chico fruit": "Manilkara zapota",
-"arrowroot": "Maranta arundinacea",
-"mint": "Mentha spp",
-"peppermint": "Mentha × piperita",
-"medlar": "Mespilus germanica",
-"bitter melon": "Momordica charantia",
-"bitter gourd": "Momordica charantia",    
-"moringa": "Moringa oleifera",
-"drumstick": "Moringa oleifera",
-"mulberry": "Morus spp",
-"banana": "Musa acuminata",
-"banana leaves": "Musa acuminata",
-"plantain": "Musa paradisiaca",
-"green banana": "Musa paradisiaca",
-"abaca": "Musa textilis",
-"rambutan": "Nephelium lappaceum",
-"tobacco": "Nicotiana tabacum",
-"basil": "Ocimum basilicum",
-"olive": "Olea europaea",
-"sainfoin": "Onobrychis viciifolia",
-"marjoram": "Origanum majorana",
-"parsnip": "Pastinaca sativa",
-"perilla": "Perilla spp",
-"avocado": "Persea americana",
-"parsley": "Petroselinum crispum",
-"common Bean": "Phaseolus vulgaris",
-"date": "Phoenix dactylifera",
-"new zealand Flax": "Phormium tenax",
-"indian gooseberry": "Phyllanthus emblica",
-"goldenberry": "Physalis peruviana",
-"groundcherry": "Physalis spp",
-"anise": "Pimpinella anisum",
-"black Pepper": "Piper nigrum",
-"pistachio": "Pistacia spp",
-"pea": "Pisum sativum",
-"chinese potato": "Plectranthus rotundifolius",
-"purslane": "Portulaca oleracea",
-"tinda": "Praecitrullus fistulosus",
-"apricot": "Prunus armeniaca",
-"plum": "Prunus domestica",
-"almond": "Prunus dulcis",
-"peach": "Prunus persica",
-"cherry": "Prunus spp",
-"guava": "Psidium guajava",
-"pomegranate": "Punica granatum",
-"pear": "Pyrus communis",
-"radish": "Raphanus sativus",
-"redcurrant": "Ribes rubrum",
-"currant": "Ribes spp",
-"wild Rose": "Rosa davurica",
-"rosemary": "Rosmarinus officinalis",
-"bramble": "Rubus spp",
-"raspberry": "Rubus spp",
-"sugarcane": "Saccharum officinarum",
-"sage": "Salvia officinalis",
-"black salsify": "Scorzonera hispanica",
-"chayote": "Sechium edule",
-"sesame": "Sesamum indicum",
-"potato": "Solanum tuberosum",
-"tomato": "Solanum lycopersicum",
-"eggplant": "Solanum melongena",
-"pepino": "Solanum muricatum",
-"argel": "Solenostemma argel",
-"sorghum": "Sorghum bicolor",
-"spinach": "Spinacia oleracea",
-"sweet Basil": "Ocimum basilicum",
-"tamarind": "Tamarindus indica",
-"desert truffle": "Terfeziaceae spp",
-"cocoa": "Theobroma cacao",
-"thyme": "Thymus vulgaris",
-"snake gourd": "Trichosanthes cucumerina",
-"trichosanthes": "Trichosanthes spp",
-"clover": "Trifolium spp",
-"fenugreek": "Trigonella foenum-graecum",
-"wheat": "Triticum spp",
-"blueberry": "Vaccinium spp",
-"corn Salad": "Valerianella locusta",
-"vanilla": "Vanilla planifolia",
-"fava Bean": "Vicia faba",
-"common Vetch": "Vicia sativa",
-"cowpea": "Vigna unguiculata",
-"grape": "Vitis vinifera",
-"tannia": "Xanthosoma sagittifolium",
-"corn": "Zea mays",
-"ginger": "Zingiber officinale",
-"jujube": "Ziziphus jujuba",
-"kumquat": "Fortunella margarita",
-"vigna": "Vigna spp",
-"passion fruit" : "Passiflora edulis",
-"blackberries" : "Rubus fruticosus",
-"blackberry" : "Rubus fruticosus",
-"beans" : "Phaseolus vulgaris",
-"courgettes" : "Cucurbita pepo",
-"ride gourd": "Luffa Acutangula",
-
-
+    "okra": "Abelmoschus esculentus",
+    "ladies finger": "Abelmoschus esculentus",
+    "sapodilla": "Achras sapota",
+    "kiwi": "Actinidia chinensis",
+    "button Mushroom": "Agaricus bisporus",
+    "mushroom": "Agaricus spp",
+    "green agave": "Agave atrovirens",
+    "tung Tree": "Aleurites spp",
+    "leek": "Allium ampeloprasum",
+    "onion": "Allium cepa",
+    "leek": "Allium porrum",
+    "garlic": "Allium sativum",
+    "chives": "Allium schoenoprasum",
+    "aloe vera": "Aloe vera",
+    "galangal": "Alpinia officinarum",
+    "cashew": "Anacardium occidentale",
+    "pineapple": "Ananas comosus",
+    "dill": "Anethum graveolens",
+    "caraway": "Anisosciadium lanatum",
+    "cherimoya": "Annona reticulata",
+    "sugar Apple": "Annona squamosa",
+    "chervil": "Anthriscus cerefolium",
+    "celery": "Apium graveolens",
+    "peanut": "Arachis hypogaea",
+    "areca Nut": "Areca catechu",
+    "horseradish": "Armoracia rusticana",
+    "arracacha": "Arracacia xanthorhiza",
+    "tarragon": "Artemisia dracunculus",
+    "breadfruit": "Artocarpus altilis",
+    "asparagus": "Asparagus officinalis",
+    "quebracho": "Aspidosperma spp",
+    "brazil Nut": "Bertholletia excelsa",
+    "beet": "Beta vulgaris",
+    "cabbage": "Brassica oleracea capitata",
+    "rapeseed": "Brassica napus",
+    "cabbage": "Brassica oleracea",
+    "broccoli": "Brassica oleracea Var.Italica",
+    "cauliflower": "Brassica oleracea var. botrytis",
+    "cabbage": "Brassica oleracea var. capitata",
+    "mustard": "Brassica spp",
+    "pigeon Pea": "Cajanus cajan",
+    "tea": "Camellia sinensis",
+    "bell Pepper": "Capsicum annuum",
+    "pepper": "Capsicum spp",
+    "capsicum":"Capsicumm annuum",
+    "papaya": "Carica papaya",
+    "caraway": "Carum carvi",
+    "pecan": "Carya illinoensis",
+    "chestnut": "Castanea sativa",
+    "kapok": "Ceiba pentandra",
+    "carob": "Ceratonia siliqua",
+    "quinoa": "Chenopodium quinoa",
+    "chickpea": "Cicer arietinum",
+    "endive": "Cichorium endivia",
+    "chicory": "Cichorium intybus",
+    "cinchona": "Cinchona spp",
+    "watermelon": "Citrullus lanatus",
+    "orange": "Citrus sinensis",
+    "lime": "Citrus aurantifolia",
+    "bitter Orange": "Citrus aurantium",
+    "bergamot": "Citrus bergamia",
+    "pomelo": "Citrus grandis",
+    "kaffir Lime": "Citrus hystrix DC",
+    "lemon": "Citrus limon",
+    "citrus medica": "Citrus medica",
+    "grapefruit": "Citrus paradisi",
+    "tangerine": "Citrus reticulata",
+    "sweet orange": "Citrus sinensis",
+    "mandarin": "Citrus veticulata",
+    "ivy gourd": "Coccinia grandis",
+    "tindly": "Coccinia grandis",
+    "coconut": "Cocos nucifera",
+    "coffee": "Coffee spp",
+    "taro": "Colocasia spp",
+    "congo jute": "Corchorus olitorius",
+    "coriander": "Coriandrum sativum",
+    "hazelnut": "Corylus avellana",
+    "melon": "Cucumis melo",
+    "honeydew melon": "Cucumis melo Inodorus",
+    "armenian cucumber": "Cucumis melo flexuosus",
+    "cucumber": "Cucumis sativus",
+    "pumpkin": "Cucurbita pepo",
+    "ash gourd": "Benincasa Hispida", 
+    "squash": "Cucurbita spp",
+    "chow chow": "Cucurbita spp",
+    "turmeric": "Curcuma longa",
+    "guar": "Cyamopsis tetragonoloba",
+    "quince": "Cydonia oblonga",
+    "lemongrass": "Cymbopogon spp",
+    "artichoke": "Cynara cardunculus",
+    "carrot": "Daucus carota",
+    "longan": "Dimocarpus longan",
+    "yam": "Dioscorea spp",
+    "persimmon": "Diospyros kaki",
+    "loquat": "Eriobotrya japonica",
+    "arugula": "Eruca Sativa",
+    "fig": "Ficus carica",
+    "fennel": "Foeniculum vulgare",
+    "strawberry": "Fragaria spp",
+    "maguey": "Furcraea macrophylla",
+    "mangosteen": "Garcinia mangostana",
+    "soybean": "Glycine max",
+    "licorice": "Glycyrrhiza glabra",
+    "grape leaves": "Vitis vinifera",
+    "niger Seed": "Guizotia abyssinica",
+    "kenaf": "Hibiscus cannabinus",
+    "barley": "Hordeum vulgare",
+    "hops": "Humulus lupulus",
+    "dragon fruit": "Hylocereus costaricensis",
+    "yerba Mate": "Ilex paraguariensis",
+    "sweet Potato": "Ipomoea batatas",
+    "walnut": "Juglans spp",
+    "lettuce": "Lactuca sativa",
+    "bottle gourd": "Lagenaria siceraria",
+    "lavender": "Lavandula spp",
+    "lentil": "Lens culinaris",
+    "cress": "Lepidium sativum",
+    "flax": "Linum usitatissimum",
+    "litchi": "Litchi chinensis",
+    "lupin": "Lupinus spp",
+    "macadamia": "Macadamia spp",
+    "apple": "Malus domestica",
+    "crab Apple": "Malus sylvestris",
+    "mango": "Mangifera indica",
+    "yuca": "Manihot esculenta",
+    "chico fruit": "Manilkara zapota",
+    "arrowroot": "Maranta arundinacea",
+    "mint": "Mentha spp",
+    "peppermint": "Mentha × piperita",
+    "medlar": "Mespilus germanica",
+    "bitter melon": "Momordica charantia",
+    "bitter gourd": "Momordica charantia",    
+    "moringa": "Moringa oleifera",
+    "drumstick": "Moringa oleifera",
+    "mulberry": "Morus spp",
+    "banana": "Musa acuminata",
+    "banana leaves": "Musa acuminata",
+    "plantain": "Musa paradisiaca",
+    "green banana": "Musa paradisiaca",
+    "abaca": "Musa textilis",
+    "rambutan": "Nephelium lappaceum",
+    "tobacco": "Nicotiana tabacum",
+    "basil": "Ocimum basilicum",
+    "olive": "Olea europaea",
+    "sainfoin": "Onobrychis viciifolia",
+    "marjoram": "Origanum majorana",
+    "parsnip": "Pastinaca sativa",
+    "perilla": "Perilla spp",
+    "avocado": "Persea americana",
+    "parsley": "Petroselinum crispum",
+    "common Bean": "Phaseolus vulgaris",
+    "date": "Phoenix dactylifera",
+    "new zealand Flax": "Phormium tenax",
+    "indian gooseberry": "Phyllanthus emblica",
+    "goldenberry": "Physalis peruviana",
+    "groundcherry": "Physalis spp",
+    "anise": "Pimpinella anisum",
+    "black Pepper": "Piper nigrum",
+    "pistachio": "Pistacia spp",
+    "pea": "Pisum sativum",
+    "chinese potato": "Plectranthus rotundifolius",
+    "purslane": "Portulaca oleracea",
+    "tinda": "Praecitrullus fistulosus",
+    "apricot": "Prunus armeniaca",
+    "plum": "Prunus domestica",
+    "almond": "Prunus dulcis",
+    "peach": "Prunus persica",
+    "cherry": "Prunus spp",
+    "guava": "Psidium guajava",
+    "pomegranate": "Punica granatum",
+    "pear": "Pyrus communis",
+    "radish": "Raphanus sativus",
+    "redcurrant": "Ribes rubrum",
+    "currant": "Ribes spp",
+    "wild Rose": "Rosa davurica",
+    "rosemary": "Rosmarinus officinalis",
+    "bramble": "Rubus spp",
+    "raspberry": "Rubus spp",
+    "sugarcane": "Saccharum officinarum",
+    "sage": "Salvia officinalis",
+    "black salsify": "Scorzonera hispanica",
+    "chayote": "Sechium edule",
+    "sesame": "Sesamum indicum",
+    "potato": "Solanum tuberosum",
+    "tomato": "Solanum lycopersicum",
+    "eggplant": "Solanum melongena",
+    "pepino": "Solanum muricatum",
+    "argel": "Solenostemma argel",
+    "sorghum": "Sorghum bicolor",
+    "spinach": "Spinacia oleracea",
+    "sweet Basil": "Ocimum basilicum",
+    "tamarind": "Tamarindus indica",
+    "desert truffle": "Terfeziaceae spp",
+    "cocoa": "Theobroma cacao",
+    "thyme": "Thymus vulgaris",
+    "snake gourd": "Trichosanthes cucumerina",
+    "trichosanthes": "Trichosanthes spp",
+    "clover": "Trifolium spp",
+    "fenugreek": "Trigonella foenum-graecum",
+    "wheat": "Triticum spp",
+    "blueberry": "Vaccinium spp",
+    "corn Salad": "Valerianella locusta",
+    "vanilla": "Vanilla planifolia",
+    "fava Bean": "Vicia faba",
+    "common Vetch": "Vicia sativa",
+    "cowpea": "Vigna unguiculata",
+    "grape": "Vitis vinifera",
+    "tannia": "Xanthosoma sagittifolium",
+    "corn": "Zea mays",
+    "ginger": "Zingiber officinale",
+    "jujube": "Ziziphus jujuba",
+    "kumquat": "Fortunella margarita",
+    "vigna": "Vigna spp",
+    "passion fruit" : "Passiflora edulis",
+    "blackberries" : "Rubus fruticosus",
+    "blackberry" : "Rubus fruticosus",
+    "beans" : "Phaseolus vulgaris",
+    "courgettes" : "Cucurbita pepo",
+    "ride gourd": "Luffa Acutangula",
 
     
     // Add more fruits and vegetables with their genus here
@@ -235,48 +233,40 @@ function searchGenus() {
 function addToTable() {
     const genus = document.getElementById('genusDisplay').innerText;
     const name = document.getElementById('searchInput').value; 
-    const quantity = document.getElementById('quantityInput').value;
-    const kilograms = document.getElementById('kilogramInput').value;
+
 
     document.getElementById('searchInput').value = '';
     document.getElementById('genusDisplay').innerText = '';
-    document.getElementById('quantityInput').value = '';
-    document.getElementById('kilogramInput').value = '';
+    
 
-    if (genus && name && quantity && kilograms) {
+    if (genus && name) {
         const table = document.getElementById('resultTable').getElementsByTagName('tbody')[0];
         const newRow = table.insertRow();
 
         const cell1 = newRow.insertCell(0);
         const cell2 = newRow.insertCell(1);
         const cell3 = newRow.insertCell(2);
-        const cell4 = newRow.insertCell(3);
-        const cell5 = newRow.insertCell(4);
-
+       
         cell1.innerText = genus;
         cell1.setAttribute('data-label','Genus');
         cell2.innerText = name;
         cell2.setAttribute('data-label', 'Name');
-        cell3.innerText = quantity;
-        cell3.setAttribute('data-label', 'Quantity');
-        cell4.innerText = kilograms;
-        cell4.setAttribute('data-label', 'Kilograms');
 
         const copyButton = document.createElement('button');
         copyButton.textContent = 'Copy';
         copyButton.onclick = function() {
             copyToClipboard(genus, copyButton);
         };
-        cell5.appendChild(copyButton);
+        cell3.appendChild(copyButton);
 
-        cell5.appendChild(document.createTextNode(' '));
+        cell3.appendChild(document.createTextNode(' '));
 
         const deleteButton = document.createElement('button');
         deleteButton.textContent = 'Delete';
         deleteButton.onclick = function() {
             deleteRow(newRow);
         };
-        cell5.appendChild(deleteButton);
+        cell3.appendChild(deleteButton);
 
         updateSummary();
     }
@@ -315,6 +305,9 @@ function calculateDifference() {
 
     document.getElementById('quantityDifference').innerText = quantityDifference;
     document.getElementById('kilogramsDifference').innerText = kilogramsDifference;
+
+
+
 }
 
 
@@ -348,29 +341,20 @@ document.getElementById('randomKilogramInput').addEventListener('keypress', func
 });
 
 
+
+
 function clearData() {
     
     document.getElementById('searchInput').value = '';
     document.getElementById('genusDisplay').innerText = '';
 
-    
-    document.getElementById('quantityInput').value = '';
-    document.getElementById('kilogramInput').value = '';
-
-    
     const table = document.getElementById('resultTable').getElementsByTagName('tbody')[0];
     table.innerHTML = '';
 
-    document.getElementById('totalQuantity').innerText = '0';
-    document.getElementById('totalKilograms').innerText = '0';
-
     
-    document.getElementById('randomQuantityInput').value = '';
-    document.getElementById('randomKilogramInput').value = '';
+   
 
-    
-    document.getElementById('quantityDifference').innerText = '0';
-    document.getElementById('kilogramsDifference').innerText = '0';
+   
 }
 
 function copyToClipboard(text, button) {
@@ -402,6 +386,7 @@ document.addEventListener("keydown", function (event){
        event.preventDefault();
     }
 });
+
 
 document.getElementById('searchInput').addEventListener('input', function() {
     const input = this.value.toLowerCase();
